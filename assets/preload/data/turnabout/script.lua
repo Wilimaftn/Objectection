70647246670f9dd0c4493aedfa3db75f0a1ce79c
Belox <159970688+Wilimaftn@users.noreply.github.com>
2024-08-04 18:16:34 +0300

    Delete assets/preload/data/turnabout/script.lua

--- assets/preload/data/turnabout/script.lua
+++
@@ -1,5 +0,0 @@
-function onCreate() 
-makeLuaText('watermark',"Ported By belox ", 0, 2, 688); -- You can edit the created by Prisma Text just dont change anything else
-    setTextSize('watermark', 20);
-    addLuaText('watermark');
-end
